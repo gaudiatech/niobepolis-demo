83977a4ea8a48b7d3093f7dfca009291159672c7
wkta-tom <tiwaszko@gmail.com>
2022-05-19 23:18:48 +0200

    adapt alternative program to the new kengi.isometric

--- niobepolis/main2.py
+++ niobepolis/main2.py
@@ -36,9 +36,61 @@ class Character(kengi.isometric.IsometricMapObject):
 avatar_m = viewer = None
 keep_going = True
 
+CgmEvent = kengi.event.CgmEvent
+EngineEvTypes = kengi.event.EngineEvTypes
+mger = None
+lu_event = paint_event = None
+
+wctrl = None
+
+
+class WorldCtrl(kengi.event.EventReceiver):
+    def __init__(self):
+        super().__init__()
+
+    def proc_event(self, ev, source=None):
+        global keep_going, viewer
+
+        if ev.type == EngineEvTypes.LOGICUPDATE:
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_UP]:
+                viewer.scroll_to(0)
+            elif keys[pygame.K_DOWN]:
+                viewer.scroll_to(2)
+            if keys[pygame.K_RIGHT]:
+                viewer.scroll_to(1)
+            elif keys[pygame.K_LEFT]:
+                viewer.scroll_to(3)
+
+        if ev.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = kengi.core.proj_to_vscreen(pygame.mouse.get_pos())
+            tx, ty = viewer.map_x(mouse_x, mouse_y, return_int=False), viewer.map_y(mouse_x, mouse_y, return_int=False)
+            print(tx, ty)
+            avatar_m.x, avatar_m.y = tx, ty
+            # print(viewer.relative_x(0, 0), viewer.relative_y(0, 0))
+            # print(viewer.relative_x(0, 19), viewer.relative_y(0, 19))
+
+        elif ev.type == pygame.KEYDOWN:
+            if ev.key == pygame.K_ESCAPE:
+                keep_going = False
+            elif ev.key == pygame.K_d and avatar_m.x < tilemap.width - 1.5:
+                ev.x += 0.1
+            elif ev.key == pygame.K_a and avatar_m.x > -1:
+                avatar_m.x -= 0.1
+            elif ev.key == pygame.K_w and avatar_m.y > -1:
+                avatar_m.y -= 0.1
+            elif ev.key == pygame.K_s and avatar_m.y < tilemap.height - 1.5:
+                avatar_m.y += 0.1
+
+        elif ev.type == pygame.QUIT:
+            keep_going = False
+
 
 def game_enter():
     global screen, tilemap, avatar_m, viewer
+
+    global mger, lu_event, paint_event, wctrl
+
     kengi.init('old_school')
 
     screen = kengi.get_surface()
@@ -58,6 +110,8 @@ def game_enter():
         left_scroll_key=pygame.K_LEFT,
         right_scroll_key=pygame.K_RIGHT
     )
+    viewer.turn_on()
+
     # using a cursor -> YE
     cursor_image = pygame.image.load("xassets/half-floor-tile.png").convert_alpha()
     cursor_image.set_colorkey((255, 0, 255))
@@ -73,40 +127,20 @@ def game_enter():
     # TODO port Pbge to kengi CogObj+EventReceiver+event system,
     #  so we can avoid using pygame.USEREVENT and viewer() like here
 
+    # - refactoring -> let's use the kengi event system
+    lu_event = CgmEvent(EngineEvTypes.LOGICUPDATE, curr_t=None)
+    paint_event = CgmEvent(EngineEvTypes.PAINT, screen=kengi.get_surface())
+    mger = kengi.event.EventManager.instance()
+    wctrl = WorldCtrl()
+    wctrl.turn_on()
+
 
 def game_update():
-    global keep_going
-
-    # gdi = pbge.wait_event()
-    for gdi in pygame.event.get():
-        viewer.check_event(gdi)
-
-        if gdi.type == pygame.USEREVENT:  # pbge.TIMEREVENT:
-            viewer()
-            kengi.flip()
-
-        elif gdi.type == pygame.MOUSEBUTTONDOWN:
-            mouse_x, mouse_y = kengi.core.proj_to_vscreen(pygame.mouse.get_pos())
-            tx, ty = viewer.map_x(mouse_x, mouse_y, return_int=False), viewer.map_y(mouse_x, mouse_y, return_int=False)
-            print(tx, ty)
-            avatar_m.x, avatar_m.y = tx, ty
-            # print(viewer.relative_x(0, 0), viewer.relative_y(0, 0))
-            # print(viewer.relative_x(0, 19), viewer.relative_y(0, 19))
-
-        elif gdi.type == pygame.KEYDOWN:
-            if gdi.key == pygame.K_ESCAPE:
-                keep_going = False
-            elif gdi.key == pygame.K_d and avatar_m.x < tilemap.width - 1.5:
-                avatar_m.x += 0.1
-            elif gdi.key == pygame.K_a and avatar_m.x > -1:
-                avatar_m.x -= 0.1
-            elif gdi.key == pygame.K_w and avatar_m.y > -1:
-                avatar_m.y -= 0.1
-            elif gdi.key == pygame.K_s and avatar_m.y < tilemap.height - 1.5:
-                avatar_m.y += 0.1
-
-        elif gdi.type == pygame.QUIT:
-            keep_going = False
+    # using kengi event system
+    mger.post(lu_event)
+    mger.post(paint_event)
+    mger.update()
+    kengi.flip()
 
 
 if __name__ == '__main__':
